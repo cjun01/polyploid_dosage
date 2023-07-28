76f5fa86ec45c00b45738d7d9bfccd2408a0d2cc
Zhe Cao <cjun01@gmail.com>
2023-07-27 22:34:46 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tetraploid_dosage",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies your library has, for example:
+        # 'numpy',
+        # 'pandas',
+        # ...
+    ],
+)
