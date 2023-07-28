27dbbed7f4e68d4fe88d6a9530353abe498f0087
Zhe Cao <cjun01@gmail.com>
2023-07-27 23:27:16 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tetraploid_dosage", "hexaploid_dosage.py"
+    name="tetraploid_dosage"
     version="0.0.1",
     packages=find_packages(),
     install_requires=[
