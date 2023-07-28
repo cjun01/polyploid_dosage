e1ce571f1d3d0b68f10435c355933e3b5857d278
Zhe Cao <cjun01@gmail.com>
2023-07-27 23:33:17 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="polyploid_dosage"
+    name="polyploid_dosage",
     version="0.0.1",
     packages=find_packages(),
     install_requires=[
